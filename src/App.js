4404ae5f80d39b9ff4d79b0ce4bb8369f8c0e132
Tejas <tejaskandri28@gmail.com>
2023-07-16 19:59:52 -0400

    PreLoader

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import PreLoader from './component/PreLoader';
 
 function App() {
   return (
-    <div className="App">
-      <h1>the grratest</h1>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/*PreLoader component*/}
+    <PreLoader />
+
+        
+    </>
+    
   );
 }
 
