b91e78674f275f72f0bf312c2ff8f551022aae5e
Tejas <87278292+TejasK28@users.noreply.github.com>
2023-07-20 23:28:07 +0000

    Mini Update

--- src/component/HomePage.js
+++ src/component/HomePage.js
@@ -3,8 +3,6 @@ import './homepage.css'
 import RotatingSquare from './Square.js';
 import InfoBlock from './InfoBlock';
 import { useRef, useEffect, useState } from 'react';
-import { preLoaderAnim } from '../animations'
-import PreLoader from './PreLoader';
 
 function HomePage()
 {
@@ -13,22 +11,23 @@ function HomePage()
     
     return (
         <>
-        
-            <div className = "homepage-container">
-            
-            <div className="rotating-square-container">
+
+        <div className="rotating-square-container">
                 <RotatingSquare />
-            </div>
+        </div>
+        
+        <div className = "homepage-container">
             
             <div className="headline-text">
                 <h1>Aspiring Software Engineer</h1>
             </div>
 
             <div className="main-text">
-                <h1>Welcome To My Portfolio</h1>
+                <h1>Welcome To My Portfolio.</h1>
             </div>
 
-        </div>        
+        </div> 
+
         </>      
     )
 }
